cef3eac2d4088cb75cf541b6134d21042b2b005f
KrishnaMurthySanthanam <52101501+KrishnaMurthySanthanam@users.noreply.github.com>
2025-10-04 20:18:39 +0200

    Update InvalidRentalPeriodException.java

--- src/main/java/com/etraveli/movierental/exception/InvalidRentalPeriodException.java
+++ src/main/java/com/etraveli/movierental/exception/InvalidRentalPeriodException.java
@@ -1,6 +1,6 @@
 package com.etraveli.movierental.exception;
 
-public class InvalidRentalPeriodException   extends RuntimeException{
+public class InvalidRentalPeriodException extends RuntimeException{
     public InvalidRentalPeriodException(int days) {
         super("Invalid rental period: " + days + " days. Rental period must be a positive integer.");
     }
